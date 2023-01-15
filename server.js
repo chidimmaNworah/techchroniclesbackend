c7816fdd09a66c4671c1bdd7bbb2671c7c36348b
Chidimma Nworah <78983600+chidimmaNworah@users.noreply.github.com>
2023-01-15 10:23:18 +0100

    the correct way to set cors headers2

--- server.js
+++ server.js
@@ -21,21 +21,21 @@ mongoose
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'https://puffizzyfrontend.vercel.app/',
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'https://puffizzyfrontend.vercel.app/',
+//     credentials: true,
+//   })
+// );
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
