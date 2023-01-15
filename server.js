ed7a6c60a19e02e5c7be56cd3fe4346c1bb6e0a2
Chidimma Nworah <78983600+chidimmaNworah@users.noreply.github.com>
2023-01-15 10:10:49 +0100

    the correct way to set cors headers

--- server.js
+++ server.js
@@ -23,10 +23,19 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://puffizzybackend.herokuapp.com',
+    origin: '*',
     credentials: true,
   })
 );
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
