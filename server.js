718f9bd7fc7ddc2e0392f0e35f02a12e9f266b61
Chidimma Nworah <78983600+chidimmaNworah@users.noreply.github.com>
2023-04-10 15:52:36 +0100

    cors url correction1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'https://github.com/chidimmaNworah/techchronicles.git',
+    origin: 'https://techchronicles.vercel.app/',
     credentials: true,
   })
 );
